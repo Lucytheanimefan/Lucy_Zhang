70e8006dd3478b34d247b2bc5458d04870c59fa8
Lucytheanimefan <spothorse9.lucy@gmail.com>
2016-06-05 15:49:16 -0700

    Added gear svg

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-$("#HerCampus").fadeIn("slow");
+var s = skrollr.init();
+
+var main_svg = d3.select("#gears")
+	.attr("width", 250)
+	.attr("height", 250)
+	.attr("transform", "rotate(150)");
+
+d3.xml("http://people.duke.edu/~lz107/Lucy_Zhang/css/img/gear.svg", function(error, documentFragment) {
+	if (error) {
+		console.log(error);
+		return;
+	}
+
+	var svgNode = documentFragment.getElementsByTagName("svg")[0];
+
+	main_svg.node().appendChild(svgNode);
+
+});
 
