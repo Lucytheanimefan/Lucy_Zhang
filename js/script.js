1b7fa7029af577400fcfffa9f95a1eb20d09b6d0
Lucytheanimefan <spothorse9.lucy@gmail.com>
2016-06-25 16:28:17 -0700

    Added bootstrap to software dev page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 var s = skrollr.init();
 
 
+function dropDownInteractivity() {
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function() {
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
+    }
+}
+
+dropDownInteractivity();
 
